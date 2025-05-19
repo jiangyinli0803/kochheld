27f318e399bcf18afa8a7e2cd43ad509989dcdcb
ridvanberisa <ridvan.berisa@protonmail.com>
2025-05-19 18:43:00 +0200

    implement security configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,13 +8,36 @@ import Home from "./pages/Home.tsx";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import AiSearch from "./components/AiSearch.tsx";
+import axios from "axios";
+import { useEffect } from "react";
 
 
 function App() {
 
-  return (
+    function login(){
+        const host:string = window.location.host === "localhost:5173" ?
+            "http://localhost:8080/"
+            :
+            window.location.origin
+        window.open(host + "/oauth2/authorization/github", "_self")
+    }
+
+    const loadUser = ()=>{
+            axios.get("/api/auth")
+              .then(response => console.log(response.data))
+    }
+    useEffect(() => {
+        loadUser()
+     }, []);
+
+
+
+    return (
     <>
         <NavBar />
+
+        <button onClick={login}>Login</button>
+
         <Container>
             <Box sx={{ bgcolor: 'white', width: '100vw',  height: '100vh' }}>
                 <Routes>
@@ -24,6 +47,9 @@ function App() {
                     {/*<Route path={'/recipe'} element={<Recipe recipe={recipe} />} />*/}
                 </Routes>
             </Box>
+
+
+
         </Container>
     </>
   )
