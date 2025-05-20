9a17a15d614e46a9c7d1ca549c909875ee283faa
Yinli <jiangyinli0803@gmail.com>
2025-05-20 10:40:25 +0200

    update OAuth Controller Test

--- backend/src/test/java/org/example/backend/security/AuthControllerTest.java
+++ backend/src/test/java/org/example/backend/security/AuthControllerTest.java
@@ -1,8 +1,10 @@
 package org.example.backend.security;
+import org.example.backend.configuration.OAuth2TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -11,6 +13,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(OAuth2TestConfig.class)
 class AuthControllerTest {
    @Autowired
     private MockMvc mockMvc;
