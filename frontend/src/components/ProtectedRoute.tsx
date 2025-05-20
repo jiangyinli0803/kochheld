3c496d654d2105e769a4c133a88bc2eea832c3d0
git-spec <inkyfischer@gmail.com>
2025-05-20 11:21:36 +0200

    Add protection to route.

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import {Navigate, Outlet} from "react-router-dom";
+
+type ProtectedRouteProps = {
+    user: string | undefined | null;
+}
+
+function ProtectedRoute(props: Readonly<ProtectedRouteProps>) {
+
+    if (props.user === undefined) <h3>loading</h3>
+
+    return(
+        <>
+            props.user ? <Outlet /> : <Navigate to={"/"} />
+        </>
+    );
+}
+
+export default ProtectedRoute;
