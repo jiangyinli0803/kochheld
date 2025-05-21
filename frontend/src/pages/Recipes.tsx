e3dd1263ddb250880cab9e485a522666786b6940
git-spec <inkyfischer@gmail.com>
2025-05-21 19:31:34 +0200

    Create getRecipes and getRecipesByCategory in Recipes.

--- frontend/src/pages/Recipes.tsx
+++ frontend/src/pages/Recipes.tsx
@@ -1,39 +1,59 @@
 import Grid from '@mui/material/Grid2';
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 import type {IRecipe} from "../interfaces/IRecipe.ts";
 import RecipeCard from "../components/RecipeCard.tsx";
-import axios from "axios";
-import {useEffect, useState} from "react";
 
+type Props = {
+    category?: string;
+};
 
-function Recipes() {
+function Recipes(props: Props) {
     const [recipes, setRecipes] = useState<IRecipe[]>();
 
     function getRecipes(): void {
-        axios.get('api/recipes')
+        axios.get('/api/recipes')
+            .then(res => setRecipes(res.data))
+            .catch(error => console.log(error));
+    }
+
+    function getRecipesBvCategory(category: string): void {
+        axios.get(`/api/recipes/${category}`)
             .then(res => setRecipes(res.data))
             .catch(error => console.log(error));
     }
 
     useEffect(() => {
-        getRecipes();
-    }, []);
+        if (!props.category) {
+            getRecipes();
+        } else {
+            console.log(props.category)
+            getRecipesBvCategory(props.category)
+        }
+    }, [props.category]);
 
     return(
         <>
-            <Grid container columnSpacing={2} rowSpacing={4}>
-                {
-                    recipes && recipes.map(recipe => {
-                        return <Grid
-                            key={recipe.id}
-                            size={{xs: 12, sm: 4, md: 3, lg: 2}}
-                            flexGrow={1}
-                        >
-                            <RecipeCard recipe={recipe} />
-                        </Grid>
-                    })
-                }
-            </Grid>
+            <Container>
+                <Box>
+                    <Grid container columnSpacing={2} rowSpacing={4}>
+                        {
+                            recipes && recipes.map(recipe => {
+                                return <Grid
+                                    key={recipe.id}
+                                    size={{xs: 12, sm: 4, md: 3, lg: 2}}
+                                    flexGrow={1}
+                                >
+                                    <RecipeCard recipe={recipe} />
+                                </Grid>
+                            })
+                        }
+                    </Grid>
+                </Box>
+            </Container>
         </>
     );
 }
