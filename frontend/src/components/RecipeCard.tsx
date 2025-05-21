ac152e8fcefb09505226ced0ca4f971dcbbe7c80
git-spec <inkyfischer@gmail.com>
2025-05-21 16:43:54 +0200

    Add Link to RecipeCard.

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -8,6 +8,7 @@ import Typography from "@mui/material/Typography";
 import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined';
 
 import type {IRecipe} from "../interfaces/IRecipe.ts";
+import {Link} from "react-router-dom";
 
 type Props = {
   recipe: IRecipe;
@@ -20,26 +21,28 @@ function RecipeCard(props: Props) {
     return (
         <>
             <Card sx={{ minWidth: {xs: '100%'}, maxWidth: {sm: 180}}}>
-                <CardActionArea href={`/recipe/${recipe.id}`}>
-                    <CardMedia
-                        component={'img'}
-                        alt={recipe.name}
-                        image={recipe.image}
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {recipe.name}
-                        </Typography>
-                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                            {recipe.description}
-                        </Typography>
-                        <Box component={'div'} sx={{display: 'flex', alignItems: 'center'}}>
-                            <Typography variant="caption" fontSize={'inherit'} sx={{color: 'text.secondary', paddingTop: '2px'}}>
-                                {recipe.duration && recipe.duration.time} {recipe.duration && recipe.duration.dimension}
+                <CardActionArea>
+                    <Link to={`/recipe/${recipe.id}`} state={{recipe}}>
+                        <CardMedia
+                            component={'img'}
+                            alt={recipe.name}
+                            image={recipe.image}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {recipe.name}
                             </Typography>
-                            <TimerOutlinedIcon fontSize={'small'} sx={{marginLeft: '.2rem'}}></TimerOutlinedIcon>
-                        </Box>
-                    </CardContent>
+                            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                {recipe.description}
+                            </Typography>
+                            <Box component={'div'} sx={{display: 'flex', alignItems: 'center'}}>
+                                <Typography variant="caption" fontSize={'inherit'} sx={{color: 'text.secondary', paddingTop: '2px'}}>
+                                    {recipe.duration && recipe.duration} Min.
+                                </Typography>
+                                <TimerOutlinedIcon fontSize={'small'} sx={{marginLeft: '.2rem'}}></TimerOutlinedIcon>
+                            </Box>
+                        </CardContent>
+                    </Link>
                 </CardActionArea>
             </Card>
         </>
