9c21c49580d0752ff5178add4347a55cb672e339
git-spec <inkyfischer@gmail.com>
2025-05-21 15:32:41 +0200

    Add query findByCategory to RecipeRepository.

--- backend/src/main/java/org/example/backend/repository/RecipeRepository.java
+++ backend/src/main/java/org/example/backend/repository/RecipeRepository.java
@@ -1,8 +1,13 @@
 package org.example.backend.repository;
 
+import org.example.backend.model.Category;
 import org.example.backend.model.Recipe;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RecipeRepository extends MongoRepository<Recipe, String>{ }
+public interface RecipeRepository extends MongoRepository<Recipe, String>{
+    List<Recipe> findByCategory(Category category);
+}
