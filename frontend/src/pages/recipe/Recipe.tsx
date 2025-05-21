9a3ce53c5414ae83c3e4d2618fb4ba91349d52c8
git-spec <inkyfischer@gmail.com>
2025-05-21 17:10:43 +0200

    Create getRecipe in Recipe and add state recipe to it.

--- frontend/src/pages/recipe/Recipe.tsx
+++ frontend/src/pages/recipe/Recipe.tsx
@@ -1,19 +1,28 @@
-import bgImage from '../../assets/images/background.png';
 import * as React from "react";
+import {useLocation, useParams} from "react-router-dom";
+import axios from "axios";
 
 import './Recipe.css';
 import type {IRecipe} from "../../interfaces/IRecipe.ts";
+import bgImage from '../../assets/images/background.png';
+import {useEffect} from "react";
 
 
-type Props = {
-    recipe: IRecipe
-}
+const Recipe: React.FC = () => {
+    const [recipe, setRecipe] = React.useState<IRecipe>();
+    const { id } = useParams();
+    const location = useLocation();
+    const entry: IRecipe = location.state?.recipe as IRecipe;
 
+    function getRecipe(): void {
+        axios.get(`api/recipe/${id}`)
+            .then(res => setRecipe(res.data))
+            .catch(error => console.log(error));
+    }
 
-
-const Recipe: React.FC<Props> = (props) => {
-
-
+    useEffect(() => {
+        if (!entry) getRecipe();
+    }, []);
 
     return (
         <div
@@ -22,20 +31,37 @@ const Recipe: React.FC<Props> = (props) => {
         >
             <div className="recipe-card">
                 <div className="top-row">
-                    <img className="image" src={props.recipe.image} alt={props.recipe.name} />
+                    <img
+                        className="image"
+                        src={entry ? entry.image : recipe && recipe.image}
+                        alt={entry ? entry.name : recipe && recipe.name} />
 
                     <div className="text-columns">
                         <div className="description">
-                            <h2 className="title">{props.recipe.name}</h2>
-                            <p>{props.recipe.description}</p>
+                            <h2 className="title">
+                                {entry ? entry.name : recipe && recipe.name}
+                            </h2>
+                            <p>
+                                {entry ? entry.description : recipe && recipe.description}
+                            </p>
                         </div>
 
                         <div className="ingredients">
                             <h3>Inhaltsstoffe:</h3>
                             <ol>
-                                {props.recipe.ingredients.map((item, index) => (
-                                    <li key={index}>{item}</li>
-                                ))}
+                                {
+                                    entry ?
+                                        entry.ingredients.map(
+                                            (item, index) => (
+                                                <li key={index}>{item}</li>
+                                            )
+                                        )
+                                        : recipe && recipe.ingredients.map(
+                                            (item, index) => (
+                                                <li key={index}>{item}</li>
+                                            )
+                                        )
+                                }
                             </ol>
                         </div>
                     </div>
@@ -43,7 +69,9 @@ const Recipe: React.FC<Props> = (props) => {
 
                 <div className="steps">
                     <h3>Zubereitung</h3>
-                    <p>{props.recipe.instruction}</p>
+                    <p>{entry ? entry.instruction : recipe && recipe.instruction}</p>
+                    &nbsp;
+                    <p>Dauer: {entry ? entry.duration : recipe && recipe.duration} Min.</p>
                 </div>
             </div>
   </div>
