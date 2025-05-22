d06de97184dbc066714a3ec772be12eed98119fa
kostenkoLiza09 <kostaz063@gmail.com>
2025-05-22 09:16:31 +0200

    update RecipeDto.java

--- backend/src/main/java/org/example/backend/model/dto/RecipeDto.java
+++ backend/src/main/java/org/example/backend/model/dto/RecipeDto.java
@@ -1,10 +1,15 @@
 package org.example.backend.model.dto;
 
+import org.example.backend.model.Category;
+
 import java.util.List;
 
 public record RecipeDto (
         String name,
-        String imageUrl,
+        String image,
         List<String> ingredients,
-        String description ) {
-}
+        String instruction,
+        String description,
+        int duration,
+        Category category)
+{ }
