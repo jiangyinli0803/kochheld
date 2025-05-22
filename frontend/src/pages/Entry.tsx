0113b8b552bbda88bfe83629486b0c93061c045a
git-spec <inkyfischer@gmail.com>
2025-05-22 20:15:37 +0200

    Add controllers to Entry.

--- frontend/src/pages/Entry.tsx
+++ frontend/src/pages/Entry.tsx
@@ -2,7 +2,7 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import {type FormEvent, useState, useMemo} from "react";
+import {type FormEvent, useState, useMemo, type ChangeEvent} from "react";
 import Select, {type SelectChangeEvent} from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
@@ -28,7 +28,12 @@ function MyFormHelperText() {
 }
 
 function Entry() {
+    const [name, setName] = useState<string>();
     const [category, setCategory] = useState<string>('');
+    const [ingredients, setIngredients] = useState<string>();
+    const [duration, setDuration] = useState<string>();
+    const [description, setDescription] = useState<string>();
+    const [instruction, setInstruction] = useState<string>();
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -36,8 +41,28 @@ function Entry() {
         alert(new URLSearchParams(formData as never).toString());
     };
 
+    function handleName(e: ChangeEvent<HTMLInputElement>) {
+        setName(e.target.value as string);
+    }
+
     function handleCategory(e: SelectChangeEvent) {
         setCategory(e.target.value as string);
+    }
+
+    function handleIngredients(e: ChangeEvent<HTMLInputElement>) {
+        setIngredients(e.target.value as string);
+    }
+
+    function handleDuration(e: ChangeEvent<HTMLInputElement>) {
+        setDuration(e.target.value as string);
+    }
+
+    function handleDescription(e: ChangeEvent<HTMLInputElement>) {
+        setDescription(e.target.value as string);
+    }
+
+    function handleInstruction(e: ChangeEvent<HTMLInputElement>) {
+        setInstruction(e.target.value as string);
     }
 
     return (
@@ -49,7 +74,14 @@ function Entry() {
                     <form onSubmit={handleSubmit}>
                         <Grid container columnSpacing={2} sx={{mt: 4, mb: 12}}>
                             <Grid size={{xs: 12, sm: 7, md: 8}} sx={{mb: 4}}>
-                                <TextField id="name" label="Name" variant="outlined" fullWidth />
+                                <TextField
+                                    id="name"
+                                    label="Name"
+                                    variant="outlined"
+                                    value={name}
+                                    onChange={handleName}
+                                    fullWidth
+                                />
                             </Grid>
                             <Grid size={{xs: 12, sm: 5, md: 4}} sx={{mb: 4}}>
                                 <Box sx={{ minWidth: 120 }}>
@@ -73,7 +105,12 @@ function Entry() {
                             <Grid size={{xs: 12, sm: 9, md: 10}} sx={{mb: 1}}>
                                 <FormControl fullWidth>
                                     <InputLabel id={'ingredients-label'}>Zutaten</InputLabel>
-                                    <OutlinedInput id="ingredients" label="Zutaten" />
+                                    <OutlinedInput
+                                        id="ingredients"
+                                        label="Zutaten"
+                                        value={ingredients}
+                                        onChange={handleIngredients}
+                                    />
                                     <MyFormHelperText />
                                 </FormControl>
                             </Grid>
@@ -88,6 +125,8 @@ function Entry() {
                                             endAdornment: <InputAdornment position="end">Min.</InputAdornment>,
                                         },
                                     }}
+                                    value={duration}
+                                    onChange={handleDuration}
                                     fullWidth
                                 />
                             </Grid>
@@ -96,6 +135,8 @@ function Entry() {
                                     id="discription"
                                     label="Kurzbeschreibung"
                                     variant="outlined"
+                                    value={description}
+                                    onChange={handleDescription}
                                     multiline
                                     fullWidth
                                 />
@@ -105,6 +146,8 @@ function Entry() {
                                     id="instruction"
                                     label="Zubereitung"
                                     variant="outlined"
+                                    value={instruction}
+                                    onChange={handleInstruction}
                                     multiline
                                     fullWidth
                                 />
